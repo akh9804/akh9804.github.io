273d17dfb799d3c97d311fcfe7e9ecfe33196a9b
ahntree <akh9804@gmail.com>
2022-03-02 18:09:42 +0900

    구글 애널리틱스 설정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -27,6 +27,6 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '0', // Add your google analytics tranking ID
+  ga: 'G-QTX33W68Q6', // Add your google analytics tranking ID
   ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
 }
